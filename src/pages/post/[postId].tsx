f2a04f3afe11fb013fb9253266a97215d73f5dd2
Alex Zani <zani.alexandr@gmail.com>
2024-05-20 23:28:18 +0300

    Modify the single Post page to render at request time.

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -40,4 +40,30 @@ export const getStaticProps: GetStaticProps<PostPageProps> = async ({
   };
 };
 
+export async function getStaticPaths() {
+  // When this is true (in preview environments) don't
+  // prerender any static pages
+  // (faster builds, but slower initial page load)
+  if (process.env.SKIP_BUILD_STATIC_GENERATION) {
+    return {
+      paths: [],
+      fallback: 'blocking',
+    };
+  }
+
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+
+  // Get the paths we want to prerender based on posts
+  // In production environments, prerender all pages
+  // (slower builds, but faster initial page load)
+  const paths = posts.map((post) => ({
+    params: { id: post.id },
+  }));
+
+  // { fallback: false } means other routes should 404
+  return { paths, fallback: false };
+}
+
 export default PostPage;
