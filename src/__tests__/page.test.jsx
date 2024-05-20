c95090479abca5946867a6a8a675377a4529d1ab
Alex Zani <zani.alexandr@gmail.com>
2024-05-20 21:12:34 +0300

    Add prettier to project.

--- src/__tests__/page.test.jsx
+++ src/__tests__/page.test.jsx
@@ -1,15 +1,15 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import Home from '../pages/index'
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/index';
 
 describe('Home', () => {
-    it('renders a heading', () => {
-        render(<Home />)
+  it('renders a heading', () => {
+    render(<Home />);
 
-        const heading = screen.getByRole('heading', { level: 1 })
-        const link = screen.getByRole('link', { name : 'About' })
+    const heading = screen.getByRole('heading', { level: 1 });
+    const link = screen.getByRole('link', { name: 'About' });
 
-        expect(heading).toBeInTheDocument()
-        expect(link).toBeInTheDocument()
-    })
-})
+    expect(heading).toBeInTheDocument();
+    expect(link).toBeInTheDocument();
+  });
+});
